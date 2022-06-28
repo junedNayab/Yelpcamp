4c98fb1f644787014b5d237747e7cced535e61d4
junedNayab <mohammednayab000@gmail.com>
2022-06-29 04:37:10 +0530

    done

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ const moment= require("moment");
 const mongoose =require("mongoose");
 const MongoStore = require('connect-mongo');
 
-
-
 const db_url= process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(db_url, {
     useNewUrlParser: true,
